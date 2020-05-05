2189f1acbaa47a30c19dbb7b0385a73cb8c61647
murielledouay <guyard_murielle@orange.fr>
2020-05-05 17:04:46 +0200

    PHP Partie 1 exo 3

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+//déclaration de la variable km avec sa valeur 1
+$km = '125';
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+  <meta charset="utf-8"/>
+  <link rel="stylesheet" href="assets/css/style.css"/>
+  <title>Partie 1 exo 3</title>
+</head>
+
+<body>
+
+  <p>Nombre de kilomètres:
+    <?php
+    //j'affiche avec echo la variable km sur mon navigateur
+    echo $km;
+    ?>
+  </p>
+
+
+
+</body>
+</html>
